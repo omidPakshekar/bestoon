0938a88b23eac60708f9d501d6b664e679784037
omid <omid.pakshekar.est@gmail.com>
2019-06-30 18:42:06 +0430

    expense model is there

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+ 
